689869a176593d9a2e6a6305f606504962b14cdd
JonasGN <j.goncalvesneto@outlook.com>
2022-05-24 16:09:25 -0300

    add SubscribeButton and Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import womanPic from "/public/images/girl-coding.svg";
+import styles from "../styles/pages/home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news | Home</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello world</h1>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <p>👏 Hey, Welcome</p>
+          <h1>
+            News about <br /> the <span>React</span> world
+          </h1>
+          <p className={styles.subscribeInfo}>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <div className={styles.detailImage}>
+          <Image src={womanPic} alt="Girl coding" />
+        </div>
+      </main>
     </>
   );
 }
