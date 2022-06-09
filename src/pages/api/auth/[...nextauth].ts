0f99c1a6ff2bcba5d3ab40cc5141c7fc231f458d
JonasGN <j.goncalvesneto@outlook.com>
2022-06-09 15:18:33 -0300

    add user persist on faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
 const env = process.env;
 
 export default NextAuth({
@@ -10,4 +13,23 @@ export default NextAuth({
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const matcher = q.Match(q.Index("user_by_email"), q.Casefold(user.email));
+      const data = { name: user.name, email: user.email };
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(matcher)),
+            q.Create(q.Collection("users"), { data }),
+            q.Get(matcher)
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
